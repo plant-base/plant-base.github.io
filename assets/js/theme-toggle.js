dec023921a771d598829303df8b11bfcba8f3127
plant-base <>
2024-03-11 16:46:10 +0000

    Update from https://github.com/plant-base/plant-base/commit/2354664034b2c20ba546ded24a04e6b53088ae5e

--- assets/js/theme-toggle.js
+++ assets/js/theme-toggle.js
@@ -1,28 +1,69 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const themeToggle = document.getElementById('theme-toggle');
-    const body = document.body;
+const storageKey = 'theme-preference';
 
+const theme = {
+    value: getThemePreference()
+};
 
-    // Function to set the theme based on stored preference
-    function setTheme(theme,) {
-        body.classList.add(theme);
+window.onload = () => {
+    const themeToggle = document.querySelector('#theme-toggle');
+    reflectThemePreference();
+    themeToggle.addEventListener('click', () => onClick());
+}
 
-        if (document.getElementById('map')) {
-            setMapTheme(theme);
-        }
+window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', ({matches: isDark}) => {
+        theme.value = isDark ? 'dark' : 'light';
+        setTheme();
+    });
 
+function onClick() {
+    if (document.body.classList.contains('dark')) {
+        theme.value = 'light';
 
-        if (theme === 'dark') {
-            // Change the SVG content and text for dark mode
-            themeToggle.innerHTML = `
+    } else {
+        theme.value = 'dark';
+    }
+    setThemePreference();
+}
+
+function getThemePreference() {
+    if (localStorage.getItem(storageKey)) {
+        return localStorage.getItem(storageKey);
+    } else {
+        return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    }
+}
+
+function setThemePreference() {
+    localStorage.setItem(storageKey, theme.value);
+    reflectThemePreference();
+}
+
+function reflectThemePreference() {
+    const themeToggle = document.querySelector('#theme-toggle');
+
+    if (theme.value === 'dark') {
+        document.body.classList.replace('light', 'dark');
+    } else if (theme.value === 'light') {
+        document.body.classList.replace('dark', 'light');
+    }
+
+    if (document.getElementById('map')) {
+        setMapTheme(theme.value);
+    }
+
+    if (theme.value === 'dark') {
+        // Change the SVG content and text for dark mode
+        themeToggle.innerHTML = `
         <svg class="moon" width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M11.3764 9.04656C11.2112 9.06022 11.0441 9.06718 10.8754 9.06718C7.56173 9.06718 4.87549 6.38093 4.87549 3.06728C4.87549 1.94653 5.18278 0.897541 5.71771 -1.22983e-05C2.52866 0.186293 0 2.83147 0 6.06725C0 9.42391 2.7211 12.145 6.07776 12.145C8.35189 12.145 10.3343 10.896 11.3764 9.04656Z" fill="currentColor"></path>
         </svg>
         <span>Low Light</span>
       `;
-        } else {
-            // Change the SVG content and text for light mode
-            themeToggle.innerHTML = `
+    } else {
+        // Change the SVG content and text for light mode
+        themeToggle.innerHTML = `
         <svg class="sun" width="17" height="16" viewBox="0 0 17 16" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="8.79932" cy="8" r="4" fill="currentColor"></circle><circle class="ray" cx="8.79932" cy="1" r="1" fill="currentColor">
             </circle><circle class="ray" cx="8.79932" cy="15" r="1" fill="currentColor">
@@ -36,30 +77,7 @@ document.addEventListener('DOMContentLoaded', function() {
         </svg>
         <span>Bright Indirect Light</span>
       `;
-        }
     }
+}
 
-    // Check if the theme preference is stored in localStorage
-    const savedTheme = localStorage.getItem('theme');
-    if (savedTheme) {
-        setTheme(savedTheme);
-    } else {
-        // If no theme preference is found, set the default theme
-        setTheme('light');
-    }
-
-    themeToggle.addEventListener('click', function() {
-        // Toggle the theme between light and dark
-        if (body.classList.contains('dark')) {
-            body.classList.replace('dark', 'light');
-            localStorage.setItem('theme', 'light');
-            setTheme('light');
-
-        } else {
-            body.classList.replace('light', 'dark');
-            localStorage.setItem('theme', 'dark');
-            setTheme('dark');
-
-        }
-    });
-});
+reflectThemePreference();
